46a237a4394cf1de80a42325b4fe0032ef0f9438
xiaoweicute <1346088365@qq.com>
2019-03-06 22:45:28 +0800

    third

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 var express = require('express');
+var AV = require('leanengine');
+
+AV.init({
+  appId: process.env.LEANCLOUD_APP_ID || 'baaPLKJpjpCo9QGk4nmBJbpc-gzGzoHsz',
+  appKey: process.env.LEANCLOUD_APP_KEY || 'uG4ajLuhmuLPhHpVB4VbOFVk',
+  masterKey: process.env.LEANCLOUD_APP_MASTER_KEY || 'ndMrpOdFWYR0rJVcN0CKExYX'
+});
 var app = express();
+app.use(AV.express())
 app.get('/',function(req, res){
     res.end('123');
 })
-app.listen(3000);
+app.listen(process.env.LEANCLOUD_APP_PORT);
